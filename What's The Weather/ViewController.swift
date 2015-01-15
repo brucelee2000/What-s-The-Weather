c0c73e011416545d956d2ad63a7a918b3ac5447f
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-15 11:20:38 -0800

    Add error check

--- What's The Weather/ViewController.swift
+++ What's The Weather/ViewController.swift
@@ -8,7 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var cityName: UITextField!
+    
+    @IBOutlet weak var blurredView: UIVisualEffectView!
+    
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    @IBAction func getWeatherButtonPressed(sender: UIButton) {
+        self.view.endEditing(true)
+       
+        let city = cityName.text
+        var cityString = city.stringByReplacingOccurrencesOfString(" ", withString: "")
+        var urlString = "http://www.weather-forecast.com/locations/" + cityString + "/forecasts/latest"
+        
+        var url = NSURL(string: urlString)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url!) { (webData, webResponse, webError) in
+            var data2Text = NSString(data: webData, encoding: NSUTF8StringEncoding)
+            
+            if data2Text!.containsString("<span class=\"phrase\">") {
+                var contentArray = data2Text?.componentsSeparatedByString("<span class=\"phrase\">") as [String]
+                var content1 = contentArray[1].componentsSeparatedByString("</span>") as [String]
+                var weather = content1[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                self.weatherLabel.text = weather
+            } else {
+                self.weatherLabel.text = "Cannot find the city - please try again"
+            }
+
+            self.blurredView.hidden = false
+            self.weatherLabel.hidden = false
+        }
+        
+        task.resume()
+        
+        println(urlString)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +57,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
 
 }
 
